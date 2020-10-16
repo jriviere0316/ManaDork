f38cc7c4316ef163f79d3905fdad3974461a1f2d
John Riviere <mr.jriviere@gmail.com>
2020-10-15 19:59:08 -0500

    working on updating user info

--- src/components/EditUser/EditUser.js
+++ src/components/EditUser/EditUser.js
@@ -8,14 +8,21 @@ const mtg = require('mtgsdk')
 class EditUser extends Component {
 
     state = {
-       
+       username: this.props.store.user.username,
+       img_url: this.props.store.user.img_url
     }
    
     editUser=() =>{
         console.log('clicked save user changes');
-        this.props.history.push('/user')
+        // this.props.history.push('/user')
     }
-   
+    
+    handleChange = (event, propertyName) => {
+        this.setState({
+            ...this.state,
+            [ propertyName ]: event.target.value
+        })
+    }
 
     render(){
         // console.log('recentCard state:',this.state.recentCard);
@@ -26,11 +33,26 @@ class EditUser extends Component {
 
                     <div id="editUserDiv">
                         <h1>EDIT USER PAGE</h1>
-                        <input placeholder="username" value={this.props.store.user.username}></input>
-                        <br/>
-                        <input placeholder="image url" value={this.props.store.user.img_url}></input> 
-                        <br/>
-                        <button onClick={this.editUser}>Save Changes</button>
+                        <form>
+                            <input 
+                            type="text"
+                            placeholder="username" 
+                            value={this.state.username} 
+                            onChange={(event)=>this.handleChange(event, 'username')}>
+                            </input>
+
+                            <br/>
+
+                            <input 
+                            type="text"
+                            placeholder="image_url" 
+                            value={this.state.img_url} 
+                            onChange={(event)=>this.handleChange(event, 'img_url')}>
+                            </input> 
+
+                            <br/>
+                            <button onClick={this.editUser}>Save Changes</button>
+                        </form>
                     </div>
                     
 
